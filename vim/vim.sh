2937f0585c937bb57790f79e8f04383fb10265f3
m1n-q <m1nq.dev@gmail.com>
2023-01-28 19:19:13 +0900

    vim sh

--- vim/vim.sh
+++ vim/vim.sh
@@ -1,20 +1,21 @@
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
+cp ./.vimrc ~/.vimrc
+mkdir -p ~/.vim/autoload/airline/themes
+mkdir -p ~/.vim/colors
 
-
-vim +PluginInstall +qall
-
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-vim +PlugInstall +qall
-
-git clone https://github.com/sainnhe/everforest.git
-
-
+# colorscheme: everforest
 git clone --depth 1 https://github.com/sainnhe/everforest.git
 
 cd everforest
 cp ./autoload/everforest.vim ~/.vim/autoload/
 cp ./colors/everforest.vim ~/.vim/colors/
 cp ./autoload/airline/themes/everforest.vim ~/.vim/autoload/airline/themes/
+
+# Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
+vim +PluginInstall +qall
+
+# vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
+
