6cfe15c08d614b57f9a4f81ce708577da6eec128
m1n-q <m1nq.dev@gmail.com>
2023-01-28 19:12:07 +0900

    vim everforest

--- vim/vim.sh
+++ vim/vim.sh
@@ -10,3 +10,11 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 vim +PlugInstall +qall
 
 git clone https://github.com/sainnhe/everforest.git
+
+
+git clone --depth 1 https://github.com/sainnhe/everforest.git
+
+cd everforest
+cp ./autoload/everforest.vim ~/.vim/autoload/
+cp ./colors/everforest.vim ~/.vim/colors/
+cp ./autoload/airline/themes/everforest.vim ~/.vim/autoload/airline/themes/
